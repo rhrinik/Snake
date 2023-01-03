f5443bf5f7a5c98d70d27e5a1189836fea939505
Erik Urban <urban@stud.uniza.sk>
2023-01-03 15:13:33 +0100

    fix snake movement

--- Shared/GameObjects/Snake.h
+++ Shared/GameObjects/Snake.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <vector>
 #include <ranges>
 #include <algorithm>
@@ -7,44 +8,55 @@
 
 class SnakeBase {
 public:
-    enum Direction { Up, Down, Left, Right };
+    enum Direction {
+        Up, Down, Left, Right
+    };
 protected:
     static int constexpr snakeSize = 40;
     Direction direction{Right};
-    std::vector<std::pair<int,int>> segments;
-    inline static std::map<Direction,std::pair<int,int>> const directionMoves = {{Up,{0,-1}},{Down,{0,1}},{Left,{-1,0}},{Right,{1,0}}};
+    std::vector<std::pair<int, int>> segments;
+    inline static std::map<Direction, std::pair<int, int>> const directionMoves = {{Up,    {0,  -1}},
+                                                                                   {Down,  {0,  1}},
+                                                                                   {Left,  {-1, 0}},
+                                                                                   {Right, {1,  0}}};
 public:
     virtual ~SnakeBase() = default;
-    explicit SnakeBase(std::pair<int,int> startingPoint) {
+
+    explicit SnakeBase(std::pair<int, int> startingPoint) {
         segments.emplace_back(startingPoint);
     }
+
     virtual void move() {
         for (std::size_t i = segments.size() - 1; i > 0; --i)
-            segments[i] = segments[i-1];
+            segments[i] = segments[i - 1];
         segments[0] += directionMoves.at(direction) * snakeSize;
     }
+
     //test
     [[nodiscard]] Direction getDirection() const {
         return direction;
     }
+
     virtual void grow() {
         segments.emplace_back(segments.back());
     }
+
     virtual void setDirection(Direction newDirection) {
-        if (segments.size() > 2 &&
-            directionMoves.at(direction) + directionMoves.at(newDirection) == std::make_pair(0,0))
-                return;
+        if (directionMoves.at(direction) + directionMoves.at(newDirection) == std::make_pair(0, 0))return;
         direction = newDirection;
     }
-    [[nodiscard]] std::vector<std::pair<int, int>> const& getSegments() const {
+
+    [[nodiscard]] std::vector<std::pair<int, int>> const &getSegments() const {
         return segments;
     }
+
     [[nodiscard]] bool selfCollision() const {
         return segments.size() > 4 &&
-            std::ranges::any_of(segments | std::views::drop(3),[&](auto const& s){return s == segments[0];});
+               std::ranges::any_of(segments | std::views::drop(3), [&](auto const &s) { return s == segments[0]; });
     }
-    [[nodiscard]] bool wallCollision(std::pair<int,int> wallTopLeft, std::pair<int,int> wallBotRight) const {
+
+    [[nodiscard]] bool wallCollision(std::pair<int, int> wallTopLeft, std::pair<int, int> wallBotRight) const {
         return segments[0].first + snakeSize > wallBotRight.first || segments[0].first < wallTopLeft.first ||
-                segments[0].second + snakeSize > wallBotRight.second || segments[0].second < wallTopLeft.second;
+               segments[0].second + snakeSize > wallBotRight.second || segments[0].second < wallTopLeft.second;
     }
 };
